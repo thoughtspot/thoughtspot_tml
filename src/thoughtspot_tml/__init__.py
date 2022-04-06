1afd9adb721d54e7f98063bab644941bfdffe81a
boonhapus <nicholas.cooper@thoughtspot.com>
2022-04-05 21:34:21 -0500

    fix pip install ability

--- src/thoughtspot_tml/__init__.py
+++ src/thoughtspot_tml/__init__.py
@@ -1,1 +1,3 @@
-from .tml import YAMLTML, Table, Answer, Liveboard, Worksheet, View
+from ._version import __version__
+from .tml import YAMLTML
+from .tml import Table, Answer, Liveboard, Worksheet, View
