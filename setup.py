5fe1bca21569a0af08659125c7970e2c039becc9
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-17 19:36:51 -0600

    installable with setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 # PEP 528 & 621 get us closer.. but this patterns allows for proper
 # install regardless of `pip install .` or `python setup.py install`
-if __name__ == '__main__':
+if __name__ == "__main__":
     setuptools.setup(
         # see setup.cfg
     )
