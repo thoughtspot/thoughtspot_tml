cd333bc6d5ca8a4dd0487ab36d74babc7f97d115
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-19 20:55:12 -0600

    add basic test with temporary data

---
+++ tests/test_sql_view.py
@@ -0,0 +1,31 @@
+from thoughtspot_tml import SQLView
+from ward import test
+
+from . import _const
+
+
+@test("SQLView deep attribute access")
+def _():
+    t = SQLView.load(_const.TESTS_DIR / "data" / "MY_SQL_VIEW.sqlview.tml")
+
+    assert type(t) is SQLView
+
+    t.guid
+    t.sql_view
+    t.sql_view.connection
+    t.sql_view.connection.fqn
+    t.sql_view.joins_with[0]
+    t.sql_view.joins_with[0].destination
+    t.sql_view.joins_with[0].on
+
+
+@test("SQLView roundtrips")
+def _():
+    before_t = (_const.TESTS_DIR / "data" / "MY_SQL_VIEW.sqlview.tml").read_text()
+    t = SQLView.loads(before_t)
+    after_t = t.dumps()
+
+    assert type(before_t) is str
+    assert type(t) is SQLView
+    assert type(after_t) is str
+    assert before_t == after_t
