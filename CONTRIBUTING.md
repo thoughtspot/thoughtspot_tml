023a86fc90e654cef8e0ee5e0b1dc5a5ce5ad110
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-20 21:23:35 -0600

    add test data location, safe public test data tbd

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,16 +1,28 @@
 # Contributing
 
+## Getting Started
+
 ```shell
 python -m venv .venv
 source .venv\bin\activate
 pip install -e .[dev]
 ```
 
-```shell
-black . --config pyproject.toml
-```
+## Build
 
 ```shell
 download edoc.proto from internal version control
 python _generate\__main__.py
+```
+
+## Linting
+
+```shell
+black . --config pyproject.toml
+```
+
+## Running Tests
+
+```shell
+coverage run -m ward; coverage report
 ```
--- tests/_const.py
+++ tests/_const.py
@@ -2,3 +2,12 @@ import pathlib
 
 
 TESTS_DIR = pathlib.Path(__file__).parent
+
+DUMMY_CONNECTION = TESTS_DIR / "data" / "DUMMY.yaml"
+DUMMY_TABLE = TESTS_DIR / "data" / "DUMMY.table.tml"
+DUMMY_VIEW = TESTS_DIR / "data" / "DUMMY.view.tml"
+DUMMY_SQL_VIEW = TESTS_DIR / "data" / "DUMMY.sqlview.tml"
+DUMMY_WORKSHEET = TESTS_DIR / "data" / "DUMMY.worksheet.tml"
+DUMMY_ANSWER = TESTS_DIR / "data" / "DUMMY.answer.tml"
+DUMMY_PINBOARD = TESTS_DIR / "data" / "DUMMY.pinboard.tml"
+DUMMY_LIVEBOARD = TESTS_DIR / "data" / "DUMMY.liveboard.tml"
