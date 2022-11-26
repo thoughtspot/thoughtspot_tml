c71a70d9d796814926351d1c98b1f952f4fd8f9d
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-26 16:43:59 -0600

    add spotapp test

--- tests/test_spotapp.py
+++ tests/test_spotapp.py
@@ -25,3 +25,17 @@ def _():
         s.save(f)
 
         assert zipfile.is_zipfile(f) is True
+
+
+@test("SpotApp TML access")
+def _():
+    s = SpotApp.read(_const.DUMMY_SPOTAPP)
+
+    assert s.manifest is not None
+    assert len(s.tml) == 5
+    assert len(s.tables) == 3
+    assert len(s.views) == 0
+    assert len(s.sql_views) == 0
+    assert len(s.worksheets) == 1
+    assert len(s.answers) == 0
+    assert len(s.liveboards) == 1
