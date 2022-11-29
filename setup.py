ead9a052f11f494c297fa404d2db45b6e93c43c9
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-29 16:14:48 -0600

    pin dependencies

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ import setuptools
 # install regardless of `pip install .` or `python setup.py install`
 if __name__ == "__main__":
     setuptools.setup(
-        # see setup.cfg
+        # see pyproject.toml
     )
