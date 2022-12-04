5c30ab3733d4434f57f3557bf2e107beccaee68f
boonhapus <nicholas.cooper@thoughtspot.com>
2022-12-04 13:08:02 -0600

    update test data for more scenarios

--- tests/_const.py
+++ tests/_const.py
@@ -3,13 +3,13 @@ import pathlib
 # fmt: off
 TESTS_DIR = pathlib.Path(__file__).parent
 DATA_DIR  = pathlib.Path(__file__).parent / "data"
-TEMP_DIR = pathlib.Path(__file__).parent / "data" / "_temp"
+TEMP_DIR  = pathlib.Path(__file__).parent / "data" / "_temp"
 # fmt: on
 
 DUMMY_CONNECTION = DATA_DIR / "DUMMY.connection.tml"
 DUMMY_TABLE = DATA_DIR / "DUMMY.table.tml"
 DUMMY_VIEW = DATA_DIR / "DUMMY.view.tml"
-DUMMY_SQL_VIEW = DATA_DIR / "DUMMY.sqlview.tml"
+DUMMY_SQL_VIEW = DATA_DIR / "DUMMY.sql_view.tml"
 DUMMY_WORKSHEET = DATA_DIR / "DUMMY.worksheet.tml"
 DUMMY_ANSWER = DATA_DIR / "DUMMY.answer.tml"
 DUMMY_PINBOARD = DATA_DIR / "DUMMY.pinboard.tml"
