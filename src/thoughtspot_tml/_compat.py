847b3fa6ad33646d127876cae54694122e305d9d
boonhapus <nicholas.cooper@thoughtspot.com>
2022-12-05 22:39:44 -0600

    fix import order for bpo40564

--- src/thoughtspot_tml/_compat.py
+++ src/thoughtspot_tml/_compat.py
@@ -1,9 +1,19 @@
-try:
+import sys
+
+if sys.version_info < (3, 8):
+    from typing_extensions import get_origin, get_args
+    from typing_extensions import Literal, TypedDict
+else:
     # AVAILABLE IN PYTHON 3.8
     from typing import get_origin, get_args
     from typing import Literal, TypedDict
-    from zipfile import Path as ZipPath
-except ImportError:
-    from typing_extensions import get_origin, get_args
-    from typing_extensions import Literal, TypedDict
+
+
+if sys.version_info < (3, 10):
+    # https://bugs.python.org/issue40564#msg377884
+    # :: zipfile.Path with several files prematurely closes zip
+    # => Use zipp 3.2.0 on Python 3.9 and earlier for the improved behavior
     from zipp import Path as ZipPath
+else:
+    # AVAILABLE IN PYTHON 3.8
+    from zipfile import Path as ZipPath
