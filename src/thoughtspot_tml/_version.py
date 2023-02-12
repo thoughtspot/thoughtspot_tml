d3c8d928975735add19cc701cfbb2154b257b594
boonhapus <nicholas.cooper@thoughtspot.com>
2023-02-11 19:56:30 -0600

    version bump --> 2.0.1

--- src/thoughtspot_tml/_version.py
+++ src/thoughtspot_tml/_version.py
@@ -1,14 +1,1 @@
-def _get_version() -> str:
-    """
-    Retrieve the version string.
-    """
-    import pathlib
-    import re
-
-    project_dir = pathlib.Path(__file__).parent.parent.parent
-    pyproject_toml = (project_dir / "pyproject.toml").read_text()
-    version = re.search(r"version = .(.*).", pyproject_toml, flags=re.M).group(1)
-    return version
-
-
-__version__ = _get_version()
+__version__ = "2.0.1"
