383af3ae29caf47adae1d05e12dc7c5e382cacc2
boonhapus <nicholas.cooper@thoughtspot.com>
2024-11-10 21:36:01 -0600

    bugfix: CDumper is faster but less forgiving on unicode

--- src/thoughtspot_tml/_yaml.py
+++ src/thoughtspot_tml/_yaml.py
@@ -99,6 +99,10 @@ def dump(document: Dict[str, Any]) -> str:
         "default_flow_style": False,
         "sort_keys": False,
         "allow_unicode": True,
-        "Dumper": _compat.Dumper,
     }
-    return yaml.dump(document, **options)
+    try:
+        return yaml.dump(document, Dumper=_compat.Dumper, **options)
+
+    # FALL BACK TO THE SLOWER PYTHON DUMPER IF WE CAN'T FULLY PARSE UNICODE
+    except UnicodeEncodeError:
+        return yaml.dump(document, Dumper=yaml.SafeDumper, **options)
